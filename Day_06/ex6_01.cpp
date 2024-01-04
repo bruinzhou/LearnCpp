177d9ecec332e19bd37a01c38aa96b38e3823130
bruinzhou <bruinzhou@outlook.com>
2024-01-04 22:23:12 +0800

    new&delete
    动态分配应用

--- Day_06/ex6_01.cpp
+++ Day_06/ex6_01.cpp
@@ -1,3 +1,26 @@
 /*
 review cpp
 */
+#include<iostream>
+
+int *f1(int n){
+    int *p,i;
+    p=new int[n];
+    for(i=0;i<n;i++) p[i]=i;
+    return p;
+}
+
+void f2(int *p,int n){
+    while(n-->0) std::cout<<*p++<<" ";
+}
+
+void f3(int *p){
+    delete [] p;
+}
+int main(){
+    int *pi;
+    pi=f1(5);
+    f2(pi,5);
+    f3(pi);
+    return 0;
+}
